512b210d8f09e60ca44232248ef5bb6401b1d9f0
alexandra <alexaspalato@gmail.com>
2019-09-25 07:41:30 +0200

    download images

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,36 @@ exports.createPages = async ({ actions, graphql }) => {
   await createPosts({ actions, graphql })
   await createSitePages({ actions, graphql })
 }
+
+/**
+ * Download WordPress images, add them to GraphQL schema.
+ * @link https://www.gatsbyjs.org/docs/node-apis/#createResolvers
+ * @link https://www.gatsbyjs.org/packages/gatsby-source-filesystem/?=#createremotefilenode
+ */
+exports.createResolvers = ({
+  actions,
+  cache,
+  createNodeId,
+  createResolvers,
+  store,
+  reporter,
+}) => {
+  const { createNode } = actions
+  createResolvers({
+    WPGraphQL_MediaItem: {
+      imageFile: {
+        type: `File`,
+        resolve(source, args, context, info) {
+          return createRemoteFileNode({
+            url: source.sourceUrl,
+            store,
+            cache,
+            createNode,
+            createNodeId,
+            reporter,
+          })
+        },
+      },
+    },
+  })
+}
