703a208679d55f860aa1093bd861d3c6c4097512
alexandra <alexaspalato@gmail.com>
2019-09-25 07:38:30 +0200

    clean

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
  * @type {{wordPressUrl: string}}
  */
 const config = {
-  wordPressUrl: `http://alexandraspalato.com/jsforwp`,
+  wordPressUrl: `http://alexandraspalato.com/webstantly`,
 }
 
 module.exports = config
